b4630deb23174e69231901bd0f349c52128a9d06
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-03-08 14:38:11 +0100

    Add Project to repository and add HelloController with test code in it

---
+++ uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/Uek2951ManoeRankApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek2951manoerank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uek2951ManoeRankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uek2951ManoeRankApplication.class, args);
+    }
+
+}
---
+++ uek295-1-manoe-rank/src/test/java/ch/noseryoung/uek2951manoerank/Uek2951ManoeRankApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek2951manoerank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uek2951ManoeRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
