b1e03443c3feff38e4a2d03c52c37eae76bd14b6
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-03-11 13:25:55 +0100

    Add annotation for pre authorize

--- uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/domain/rank/RankController.java
+++ uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/domain/rank/RankController.java
@@ -3,6 +3,7 @@ package ch.noseryoung.uek2951manoerank.domain.rank;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,29 +21,33 @@ public class RankController {
 
 
     @GetMapping("/all")
+    @PreAuthorize("hasAuthority('GET')")
     public List<Rank> getAll() {
         return service.getAll();
     }
 
-
     @GetMapping("/{rankId}")
+    @PreAuthorize("hasAuthority('GET')")
     public ResponseEntity<Rank> getById(@PathVariable("rankId") int id) {
         Rank rank = service.getById(id);
         return ResponseEntity.ok().body(service.getById(id));
     }
 
     @PostMapping
-    public ResponseEntity<Rank> addRank(Rank newRank) {
+    @PreAuthorize("hasAuthority('POST')")
+    public ResponseEntity<Rank> addRank(@RequestBody Rank newRank) throws InstanceAlreadyExistsException {
         return ResponseEntity.status(201).body(service.addRank(newRank));
     }
 
-    @PutMapping("/{rankId")
-    public ResponseEntity<Rank> updateRank(@PathVariable("rankId") int id, Rank rank) throws InstanceAlreadyExistsException {
+    @PutMapping("/{rankId}")
+    @PreAuthorize("hasAuthority('PUT')")
+    public ResponseEntity<Rank> updateRank(@PathVariable("rankId") int id, @RequestBody Rank rank) throws InstanceAlreadyExistsException {
         return ResponseEntity.status(200).body(service.updateRank(rank, id));
     }
 
 
-    @DeleteMapping("/{rankId")
+    @DeleteMapping("/{rankId}")
+    @PreAuthorize("hasAuthority('DELETE')")
     public String deleteRank(@PathVariable("rankId") int id) {
         service.deleteRank(id);
         return "The id " + id + " has been deleted";
