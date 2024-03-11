9caaeac38e808f54d235f75e131448efbef948db
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-03-11 13:32:51 +0100

    Add Role entity

---
+++ uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/domain/role/Role.java
@@ -0,0 +1,35 @@
+package ch.noseryoung.uek2951manoerank.domain.role;
+
+
+import ch.noseryoung.uek2951manoerank.domain.authority.Authority;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_Id")
+    private Integer roleId;
+
+    @Column(name = "name")
+    private String roleName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "rank_roles_authorities",
+            joinColumns = {
+                    @JoinColumn(name = "id_role", referencedColumnName = "role_id"),
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "id_authority", referencedColumnName = "authority_id")
+            }
+    )
+    private Set<Authority> roleAuth; // Join of role and authority.
+}
