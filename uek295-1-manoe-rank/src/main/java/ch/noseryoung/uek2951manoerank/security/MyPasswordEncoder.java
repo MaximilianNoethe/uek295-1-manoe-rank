a2f3d776ac3f472cb12e119e2a9716015cdfead3
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-03-11 13:39:22 +0100

    Add MyPasswordEncoder class

---
+++ uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/security/MyPasswordEncoder.java
@@ -0,0 +1,17 @@
+package ch.noseryoung.uek2951manoerank.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class MyPasswordEncoder {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // For developement use only: Don't encode passwords in database
+		// return new BCryptPasswordEncoder();    // Real world example: Encode (hash) passwords using BCrypt
+    }
+}
