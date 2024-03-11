a242a6e6a95a9a373e2a0fd4405c3b95d4184a01
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-03-11 13:27:43 +0100

    Add authority entity

---
+++ uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/domain/authority/Authority.java
@@ -0,0 +1,20 @@
+package ch.noseryoung.uek2951manoerank.domain.authority;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "authority")
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "authority_Id")
+    private Integer auhtorityId;
+
+    @Column(name = "name")
+    private String authorityName;
+}
