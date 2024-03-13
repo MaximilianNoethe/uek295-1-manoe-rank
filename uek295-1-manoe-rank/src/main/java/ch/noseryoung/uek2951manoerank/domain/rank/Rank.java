54d2f301adeef26d814dffb315fe7fbab9230be8
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-03-13 21:27:11 +0100

    Change constructor and add validations

--- uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/domain/rank/Rank.java
+++ uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/domain/rank/Rank.java
@@ -2,8 +2,13 @@ package ch.noseryoung.uek2951manoerank.domain.rank;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Singular;
 
 
 @Getter
@@ -17,27 +22,27 @@ public class Rank {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-
+    @NotNull
     @Column(name = "rank")
     private int rank;
 
-
+    @NotNull
+    @PositiveOrZero
     @Column(name = "buy_count")
     private int buyCount;
 
 
     @Column(name = "book_name")
-
+    @Size(min = 1, max = 200, message = "Your book surpasses the character limit.")
     private String bookName;
 
 
-    public Rank(int id, int rank, int buyCount, String bookId) {
-        this.id = id;
+    public Rank(int rank, int buyCount, String bookName) {
         this.rank = rank;
         this.buyCount = buyCount;
-        this.bookName = bookId;
-
+        this.bookName = bookName;
     }
+
 
     public Rank() {
 
