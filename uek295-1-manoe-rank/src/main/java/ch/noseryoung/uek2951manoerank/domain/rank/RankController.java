c12b933ef392b0c3b3473657b34458dd0413d2ef
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-03-14 10:38:36 +0100

    Update exception handlers and add @Operation

--- uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/domain/rank/RankController.java
+++ uek295-1-manoe-rank/src/main/java/ch/noseryoung/uek2951manoerank/domain/rank/RankController.java
@@ -1,16 +1,20 @@
 package ch.noseryoung.uek2951manoerank.domain.rank;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.management.InstanceAlreadyExistsException;
+import javax.management.InstanceNotFoundException;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Objects;
 
 
 @RestController
@@ -22,45 +26,59 @@ public class RankController {
 
     @GetMapping("/all")
     @PreAuthorize("hasAuthority('GET')")
-    public List<Rank> getAll() {
+    @Operation(summary = "Get all ranks", description = "Takes all rated book and returns a JSON with the status code 200")
+    public List<Rank> getAll() { // -> List -> A list from Rank
         return service.getAll();
     }
 
     @GetMapping("/{rankId}")
     @PreAuthorize("hasAuthority('GET')")
-    public ResponseEntity<Rank> getById(@PathVariable("rankId") int id) {
+    @Operation(summary = "Get a rank by id", description = "Takes a singular ranked book and returns a JSON with the status code 200")
+    public ResponseEntity<Rank> getById(@PathVariable("rankId") int id) throws InstanceNotFoundException {
         Rank rank = service.getById(id);
         return ResponseEntity.ok().body(service.getById(id));
     }
 
     @PostMapping
     @PreAuthorize("hasAuthority('POST')")
-    public ResponseEntity<Rank> addRank(@RequestBody Rank newRank) throws InstanceAlreadyExistsException {
+    @Operation(summary = "Creates a new rank", description = "Creates a new rank and provide it with an auto generated id")
+    public ResponseEntity<Rank> addRank(@Valid @RequestBody Rank newRank) throws InstanceAlreadyExistsException {
         return ResponseEntity.status(201).body(service.addRank(newRank));
     }
 
     @PutMapping("/{rankId}")
     @PreAuthorize("hasAuthority('PUT')")
-    public ResponseEntity<Rank> updateRank(@PathVariable("rankId") int id, @RequestBody Rank rank) throws InstanceAlreadyExistsException {
+    @Operation(summary = "Updates rank by id", description = "Updates the data of selected rank by its id and returns a JSON with the status code 200")
+    public ResponseEntity<Rank> updateRank(@Valid @PathVariable("rankId") int id, @RequestBody Rank rank) throws InstanceNotFoundException, InstanceAlreadyExistsException {
         return ResponseEntity.status(200).body(service.updateRank(rank, id));
     }
 
 
     @DeleteMapping("/{rankId}")
     @PreAuthorize("hasAuthority('DELETE')")
-    public String deleteRank(@PathVariable("rankId") int id) {
+    @Operation(summary = "Deletes a rank by id", description = "Deletes a rank by its id and returns a string that says \"The id \" + id + \" has been deleted\"")
+    public String deleteRank(@Valid @PathVariable("rankId") int id) throws InstanceNotFoundException {
         service.deleteRank(id);
         return "The id " + id + " has been deleted";
     }
 
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> rankNoSuchElementException(NoSuchElementException nsee) { // nsee -> NoSuchElementException
-        return ResponseEntity.status(404).body(nsee.getMessage());
-    }
 
     @ExceptionHandler(InstanceAlreadyExistsException.class)
     public ResponseEntity<String> rankInstanceAlreadyExistsException(InstanceAlreadyExistsException iaee) { // iaee -> InstanceAlreadyExistsException
         return ResponseEntity.status(409).body(iaee.getMessage());
     }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> rankMethodArgumentNotValidException(MethodArgumentNotValidException manve) {
+        return ResponseEntity.status(400).body((manve.getFieldError()).getDefaultMessage());
+    }
+
+
+    @ExceptionHandler(InstanceNotFoundException.class)
+    public ResponseEntity<String> rankInstanceNotFoundException(InstanceNotFoundException infe) {
+        return ResponseEntity.status(404).body(infe.getMessage());
+    }
+
 
 }
